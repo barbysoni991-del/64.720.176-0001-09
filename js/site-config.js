e8b5a01e486d190577d6cc640bbbf558c3cdfcb9
barbysoni991-del <barbysoni991@gmail.com>
2026-05-15 01:11:20 -0300

    Add files via upload

---
+++ js/site-config.js
@@ -0,0 +1,12 @@
+/**
+ * Dados globais do blog (header, rodapé, contato).
+ */
+const siteConfig = {
+  nomeBlog: "Enzo Rezende do Nascimento",
+  razaoSocial: "64.720.176 Enzo Rezende do Nascimento",
+  cnpj: "64.720.176/0001-09",
+  endereco:
+    "64.720.176 Enzo Rezende do Nascimento\nEstrada Dos Bandeirantes 29192\nVargem Grande\nRio de Janeiro RJ\n22785-091",
+  email: "adailtontekno@hotmail.com",
+  telefone: "(21) 3416-0495",
+};
